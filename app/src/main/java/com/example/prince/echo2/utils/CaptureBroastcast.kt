71b4ae540d49af83aec8fbe26c016d04b706b645
princeramkot <32639995+princeramkot@users.noreply.github.com>
2020-11-10 20:36:34 +0530

    android music app

---
+++ app/src/main/java/com/example/prince/echo2/utils/CaptureBroastcast.kt
@@ -0,0 +1,53 @@
+package com.example.prince.echo2.utils
+
+import android.app.Service
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.telephony.TelephonyManager
+import com.example.prince.echo2.Activities.MainActivity
+import com.example.prince.echo2.Fragment.SongPlayingFragment
+import com.example.prince.echo2.R
+
+
+class CaptureBroastcast : BroadcastReceiver() {
+    override fun onReceive(context: Context?, p1: Intent?) {
+
+        if (p1?.action == Intent.ACTION_NEW_OUTGOING_CALL) {
+            try {
+                MainActivity.Statified.notificationManager?.cancel(2000)
+            }
+                catch (e: Exception){
+                    e.printStackTrace()
+                }
+        try{
+        if (SongPlayingFragment.Statified.mediaplayer?.isPlaying as Boolean) {
+                    SongPlayingFragment.Statified.mediaplayer?.pause()
+                    SongPlayingFragment.Statified.playpauseImageButton?.setBackgroundResource(R.drawable.play_icon)
+                }
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+
+        } else {
+            val tm: TelephonyManager = context?.getSystemService(Service.TELEPHONY_SERVICE) as TelephonyManager
+            when (tm?.callState) {
+                TelephonyManager.CALL_STATE_RINGING -> {
+                    try {
+                        MainActivity.Statified.notificationManager?.cancel(2000)
+                        if (SongPlayingFragment.Statified.mediaplayer?.isPlaying as Boolean) {
+                            SongPlayingFragment.Statified.mediaplayer?.pause()
+                            SongPlayingFragment.Statified.playpauseImageButton?.setBackgroundResource(R.drawable.play_icon)
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+                else -> {
+
+                }
+            }
+
+        }
+    }
+}
